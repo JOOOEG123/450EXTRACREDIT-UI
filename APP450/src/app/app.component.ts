6a80424f309cf9c21e389e8e68aa30678e6666db
JOOOEG123 <57308580+JOOOEG123@users.noreply.github.com>
2022-04-10 18:11:49 -0400

    UP

--- APP450/src/app/app.component.ts
+++ APP450/src/app/app.component.ts
@@ -12,13 +12,14 @@ const clone = (o) => JSON.parse(JSON.stringify(o));
 export class AppComponent implements OnInit {
   publish: any[] = [];
   viewItem: any[];
+  subData: Subscription;
   private getData(path = '') {
     const baseUrl = 'http://localhost:8080';
     const url = `${baseUrl}${path}`;
     return this.http.get(url);
   }
   subscribe: Subscription[] = [];
-  allKeys = ['title', 'type', 'summary', 'year'];
+  allKeys = ['title', 'type', 'summary', 'year', 'author', 'publicationid'];
   constructor(private http: HttpClient, private formbuilder: FormBuilder) {}
   title = 'APP450';
   formControls = this.formbuilder.group({
@@ -27,21 +28,62 @@ export class AppComponent implements OnInit {
     type: '',
     summary: '',
     year: '',
+    id: '',
   });
+  ispub = true;
+  allIds = [];
   ngOnInit(): void {
-    this.getData().subscribe((data: any[]) => {
-      console.log(data);
-      this.publish = data;
-      this.viewItem = clone(data);
-    });
+    this.searchFilter();
     this.subscribe.push(
       this.formControls.get('search').valueChanges.subscribe((value) => {
         this.viewItem = this.publish.filter((item) => {
           return this.allKeys.some((key) => {
-            return item[key].toLowerCase().includes(value.toLowerCase());
+            return item[key]?.toLowerCase()?.includes(value?.toLowerCase());
           });
         });
       })
     );
+    this.subscribe.push(
+      this.formControls.get('id').valueChanges.subscribe((value) => {
+        if (!value) {
+          this.searchFilter();
+        } else {
+          this.getById(value);
+        }
+      })
+    );
+  }
+  getById(id: any) {
+    let url = `/paper${this.ispub ? '' : '/author'}/${id}`;
+    this.getData(url).subscribe((data) => {
+      console.log(data);
+      this.viewItem = [data];
+    });
+  }
+  searchFilter() {
+    this.subData?.unsubscribe();
+    this.subData = this.getData().subscribe((data: any[]) => {
+      console.log(this.allIds);
+      this.publish = data;
+      this.viewItem = clone(data);
+      console.log(data);
+      this.allIds = [];
+      data.forEach((o) => {
+        if (!this.allIds.includes(o.publicationid)) {
+          this.allIds.push(o.publicationid);
+        }
+      });
+    });
+  }
+  switchSearch() {
+    if (this.ispub) {
+      this.searchFilter();
+    } else {
+      this.getData('/paper/author').subscribe((data) => {
+        this.publish = clone(data);
+        this.viewItem = clone(data);
+      });
+    }
+    this.formControls.reset();
   }
 }
